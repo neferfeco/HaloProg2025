f93b525b057fc569f7ae135298bda7d97f80ece9
Mona Lisa <fpiller@gmail.com>
2025-09-10 12:22:33 +0200

    szam lista generálás

---
+++ elso.py
@@ -0,0 +1,24 @@
+import random
+
+
+# Lista létrehozása
+szamok = []
+
+# Lista feltöltése 100 db random kétjegyű egész számmal
+for i in range(100):
+    szam = random.randint(10, 99)
+    szamok.append(szam)
+
+# Ellenőrzés
+print(szamok)
+
+
+
+
+
+
+
+
+
+
+
